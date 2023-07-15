72323738d825fe9299d8fd5a962c24a561343508
yara-fe <yarafe95@gmail.com>
2023-07-15 12:21:04 +1000

    Added userapp

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from .models import NewsStory
 from .forms import StoryForm
 
+
 class IndexView(generic.ListView):
     template_name = 'news/index.html'
     context_object_name = "all_stories"
@@ -15,14 +16,23 @@ class IndexView(generic.ListView):
         context = super().get_context_data(**kwargs)
         context['latest_stories'] = NewsStory.objects.all()[:4]
         return context
-    
+
 class StoryView(generic.DetailView):
     model = NewsStory
     template_name = 'news/story.html'
-    context_object_name = 'story'
+    Context_object_name = 'story'
+
+class StoryView(generic.DeleteView):
+    model = NewsStory
+    template_name = 'news/story.html'
+    context_object_name='story'
 
 class AddStoryView(generic.CreateView):
     form_class = StoryForm
     context_object_name = 'storyform'
     template_name = 'news/createStory.html'
     success_url = reverse_lazy('news:index')
+
+    # def form_valid(self, form):
+    #     form.instance.author = self.request.user
+    #     return super().form_valid(form)
---
+++ she_codes_news/users/views.py
@@ -0,0 +1,11 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.views import generic
+from .models import CustomUser
+from .forms import CustomUserCreationForm
+
+# Create your views here.
+class CreateAccountView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'users/createAccount.html'
