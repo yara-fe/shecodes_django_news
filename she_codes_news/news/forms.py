4d492b37d1f4b9f242930bd4e8e08bf5b54b9724
yara-fe <yarafe95@gmail.com>
2023-07-23 22:34:41 +1000

    Set pub_date to today fo new & edited stories

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -5,27 +5,27 @@ from .models import NewsStory
 class StoryForm(ModelForm):
     class Meta: 
         model = NewsStory
-        fields = ['title','image', 'pub_date', 'content']
-        widgets = {
-            'pub_date': forms.DateInput(
-                    format='%m/%d/%Y',
-                    attrs={
-                        'class':'form-control',
-                        'placeholder':'Select a date',
-                        'type':'date'
-                    }
-                ),
-            }
+        fields = ['title','image', 'content']
+
+    # Add calendar input to set publish date
+        # widgets = {
+        #     'pub_date': forms.DateInput(
+        #             format='%m/%d/%Y',
+        #             attrs={
+        #                 'class':'form-control',
+        #                 'placeholder':'Select a date',
+        #                 'type':'date'
+        #             }
+        #         ),
+        #     }
         
+        # Set required fields when updating and adding story
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
-            # Set fields required for UpdateStoryView and AddStoryView
             if self.instance.pk:
                 self.fields['title'].required = True
-                self.fields['pub_date'].required = True
                 self.fields['content'].required = True
             else:
                 # For DeleteStoryView, make fields not required
                 self.fields['title'].required = False
-                self.fields['pub_date'].required = False
                 self.fields['content'].required = False
--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -8,5 +8,5 @@ class NewsStory(models.Model):
         on_delete=models.CASCADE
     )
     image = models.URLField(max_length=200, null=True, blank=True) #Add a new field to have images
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField(auto_now=True)  # Set auto_now=True for automatic date and time
     content = models.TextField()
